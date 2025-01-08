c98e70cce903bd5788a1035ce5ef16e5b85ac9c6
Max Bosher <164942589+max-bosher@users.noreply.github.com>
2025-01-08 15:19:37 +0000

    Update Passing Map for Match.R

--- Visualisations/Passing Map for Match.R
+++ Visualisations/Passing Map for Match.R
@@ -30,7 +30,7 @@ create_Pitch(goaltype = "box",
              line_colour = "#B3CED9", 
              background_colour = "#224C56", 
              goal_colour = "#15393D") +
-  # Plot successful and unsuccessful passes
+  #Plot passes
   geom_segment(data = passes_map_data %>% filter(!pass.goal_assist),
                aes(x = location.x, y = location.y,
                    xend = pass.end_location.x, yend = pass.end_location.y,
@@ -43,7 +43,7 @@ create_Pitch(goaltype = "box",
                    xend = pass.end_location.x, yend = pass.end_location.y,colour = "Assist"),
                lineend = "round", linewidth = 0.5,
                arrow = arrow(length = unit(0.07, "inches"), ends = "last", type = "open")) +
-  #Add Labels
+  #Add titles
   labs(title = player_analysed, subtitle = "Passing Map for Leicester City vs Man City 02/01/2016", colour = "Pass Outcome") +
   scale_y_reverse() +
   #Scale to correct size as done by Statsbomb
@@ -51,17 +51,16 @@ create_Pitch(goaltype = "box",
   #Select correct colours for legend
   scale_colour_manual(values = c("Successful" = "green", "Unsuccessful" = "red", "Assist" = "gold")) +
   #Make whole plot the same colour with white text
-  theme_minimal(base_size = 14) +
+  theme_minimal(base_size = 16) +
   theme(
     plot.background = element_rect(fill = "#224C56", color = NA),
     panel.background = element_rect(fill = "#224C56", color = NA),
     legend.background = element_rect(fill = "#224C56", color = NA),
     legend.text = element_text(color = "white"),
     legend.title = element_text(color = "white"),
-    plot.title = element_text(color = "white", hjust = 0.5, size = 16),
-    plot.subtitle = element_text(color = "white", hjust = 0.5, size = 12),
+    plot.title = element_text(color = "white", hjust = 0.5, size = 22),
+    plot.subtitle = element_text(color = "white", hjust = 0.5, size = 16),
     axis.text = element_blank(),
     axis.title = element_blank(),
     axis.ticks = element_blank(),
     panel.grid = element_blank())
-
