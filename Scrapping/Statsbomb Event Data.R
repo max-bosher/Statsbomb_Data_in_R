334bba2c5a234a08b67192332bf48a7d91083699
Max Bosher <164942589+max-bosher@users.noreply.github.com>
2025-02-02 22:48:24 +0000

    More Efficient code Code for changing names

--- Scrapping/Statsbomb Event Data.R
+++ Scrapping/Statsbomb Event Data.R
@@ -1,5 +1,5 @@
 #clean environment
-rm(list = ls()) 
+rm(list = ls())
 
 #----load packages----
 
@@ -11,27 +11,22 @@ library(tidyverse)
 #pull all free competitions from Statsbomb
 free_comp <- (FreeCompetitions())
 
-#get all free prem matches from 2015/16 season
+#get all free prem games from 2015/16
 prem_comp <- free_comp %>%
   filter(competition_id == 2 & season_id == 27)
-matches_analysed <- FreeMatches(prem_comp)
+prem_matches <- FreeMatches(prem_comp)
 #get all event data
-uncleaned_data <- free_allevents(matches_analysed)
-match_event_data <- allclean(uncleaned_data)
+prem_data <- free_allevents(prem_matches)
+prem_clean <- allclean(prem_data)
+prem_lineups <- StatsBombFreeLineups(prem_matches)
 
-#----Changing to Nicknames----
+#Save matches as rds
+saveRDS(prem_matches, file = "prem_matches_15-16.rds")
 
-#Empty list to store the results
-lineups_list <- list()
-
-#Loop to get lineups for all needed matches
-for (i in 1:nrow(matches_analysed)) {lineups_list[[i]] <- get.lineupsFree(matches_analysed[i, ])}
-
-#Combine the lineup data from all matches into one df
-lineups <- as_tibble(do.call(rbind, lineups_list))
+#----Change to Nicknames----
 
 #Remove the nests in the lineups df
-unnested_lineups <- lineups %>%
+unnested_lineups <- prem_lineups %>%
   unnest(lineup) %>%
   unnest(positions) %>%
   select(match_id, team.name = team_name, team.id = team_id, player.name = player_name,
@@ -44,23 +39,16 @@ player_nicknames <- unnested_lineups %>%
   select(player.name, player.id) %>%
   distinct()
 
-#Change player name to nickname for all relevant players for lineups df
-unnested_lineups <- unnested_lineups %>%
-  left_join(player_nicknames, by = "player.id") %>%
-  mutate(player.name = coalesce(player.name.y, player.name.x)) %>%
-  select(-player.name.x, -player.name.y)
-
-#Change player name to nickname for all relevant players for event df
-match_event_data <- match_event_data %>%
+#Update the names
+prem_clean <- prem_clean %>%
+  # First join to update player.name using player.id
   left_join(player_nicknames, by = "player.id") %>%
   mutate(player.name = coalesce(player.name.y, player.name.x)) %>%
   select(-player.name.x, -player.name.y) %>%
   left_join(player_nicknames, by = c("pass.recipient.id" = "player.id")) %>%
-  mutate(pass.recipient.name = player.name.y) %>%
+  mutate(pass.recipient.name = coalesce(player.name.y, pass.recipient.name)) %>%
   select(-player.name.y) %>%
   rename(player.name = player.name.x)
 
-#----Save as RDS----
-
-write_rds(match_event_data, file = "prem_event_data_15-16.rds")
-write_rds(matches_analysed, file = "prem_matches_15-16.rds")
+#Save event data as rds
+saveRDS(prem_clean, file = "prem_event_data_15-16.rds")
